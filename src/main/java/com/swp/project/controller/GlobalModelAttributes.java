2c074951044b7b750c7f036c84943bc2c6733dc5
Nghia <nghianguyentin99@gmail.com>
2025-11-06 17:38:13 +0700

    update edit setting for admin

---
+++ src/main/java/com/swp/project/controller/GlobalModelAttributes.java
@@ -0,0 +1,34 @@
+package com.swp.project.controller;
+
+import com.swp.project.service.SettingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@RequiredArgsConstructor
+@ControllerAdvice
+public class GlobalModelAttributes {
+    private final SettingService settingService;
+
+    @ModelAttribute("shopName")
+    public String shopName() {
+        return settingService.getShopName();
+    }
+    @ModelAttribute("shopEmail")
+    public String shopEmail() {
+        return settingService.getShopEmail();
+    }
+    @ModelAttribute("shopAddress")
+    public String shopAddress() {
+        return settingService.getShopAddress();
+    }
+    @ModelAttribute("shopPhone")
+    public String shopPhone() {
+        return settingService.getShopPhone();
+    }
+    @ModelAttribute("shopSlogan")
+    public String shopSlogan() {
+        return settingService.getShopSlogan();
+    }
+
+}
